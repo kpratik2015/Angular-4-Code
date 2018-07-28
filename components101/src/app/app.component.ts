269863141e40633adc7cf7dcc5f6bfb1d4fab692
kpratik2015 <frek1546@gmail.com>
2018-07-28 17:57:15 +0530

    component101

--- components101/src/app/app.component.ts
+++ components101/src/app/app.component.ts
@@ -2,9 +2,37 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>Hey {{ myObject.location }}</h1>
+  
+  <ul>
+    <li *ngFor = "let arr of myArr"> {{ arr }} </li>  
+  </ul>
+  
+  <ol>
+    <li *ngIf="myArr"> Yeah I exist </li>
+    <li *ngIf="!myArr"> Yeah I don't exist </li>
+    <li *ngIf="myArr == 'something'"> It does not equal so won't show </li>
+    <li *ngIf="!myArr; else otherTmpl"> It does not equal so won't show </li>
+
+    <ng-template #otherTmpl> No, I do. </ng-template>
+
+    <div *ngIf="myArr; then templ1 else templ2">Will not get shown here so don't insert anything </div>
+    
+    <ng-template #templ1> <p>Template 1</p> </ng-template>
+    <ng-template #templ2> <p>Template 2</p> </ng-template>
+
+  </ol>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'components101';
+  title = 'components101'
+  myObject = {
+    gender: 'male',
+    age: 22,
+    location: 'Pune'
+  }
+  myArr = ['Hello','World','Wassup','?']
+  ;
 }
