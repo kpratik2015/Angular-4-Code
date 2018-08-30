d87b244a102ea8a93d133fce2f7d93e1d78fa5d9
kpratik2015 <frek1546@gmail.com>
2018-08-31 00:10:17 +0530

    Styles

--- components101/src/app/favorite.component.ts
+++ components101/src/app/favorite.component.ts
@@ -3,19 +3,27 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  // inline styles: 
+  // this overwrites css of favorite.component.css because it is written after styleUrls here
+  // so order matters. The one that comes last, angular picks style of that
+  styles: [
+    ` 
+    .glyphicon {
+        color: green;
+    }
+    .glyphicon-star {
+      background: black;
+    }
+    `
+  ]
 })
 
-export class FavoriteComponent implements OnInit {
+export class FavoriteComponent {
   @Input() isFavorite: boolean;
 
   @Output() change = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
-
-  }
   onClick() {
     this.isFavorite = !this.isFavorite;
     this.change.emit({ newValue: this.isFavorite }); // notifying others something has happened. Raise an event.
