dfaf297df1053a2efe3dd03d047c95968bc671e2
kpratik2015 <frek1546@gmail.com>
2018-09-01 01:38:58 +0530

    View encapsulation or shadow dom

--- components101/src/app/favorite.component.ts
+++ components101/src/app/favorite.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css'],
+  /*
+  with emulated angular emulates shadow dom since most browsers don't support shadow dom
+  with native a shadow-root will pop up in html code making above style only applied to the template
+  To include bootstrap we need to put import bootstrap statement in above style
+  */
+  // encapsulation: ViewEncapsulation.Native // shouldn't be done
   // inline styles: 
   // this overwrites css of favorite.component.css because it is written after styleUrls here
   // so order matters. The one that comes last, angular picks style of that
-  styles: [
-    ` 
-    .glyphicon {
-        color: green;
-    }
-    .glyphicon-star {
-      background: black;
-    }
-    `
-  ]
+  // styles: [
+  //   ` 
+  //   .glyphicon {
+  //       color: green;
+  //   }
+  //   .glyphicon-star {
+  //     background: black;
+  //   }
+  //   `
+  // ]
 })
 
 export class FavoriteComponent {
