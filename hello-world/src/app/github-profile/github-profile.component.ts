ac75f549f62cc5211536cf1b784ab4b146dbf940
Pratik <frek1546@gmail.com>
2018-09-30 02:01:06 +0530

    Why Route parameters are observable

--- hello-world/src/app/github-profile/github-profile.component.ts
+++ hello-world/src/app/github-profile/github-profile.component.ts
@@ -13,15 +13,34 @@ export class GithubProfileComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) { }
 
+
+  /*
+  Why is paramMap observable?
+  In Angular, it does not makes sense to destroy one component when navigating
+  to another component. That's why there is one component and several
+  route parameters.
+
+  An observable is basically a collection of asynchronous data that
+  arrives overtime.
+  */
+
   ngOnInit() {
-    this.route.paramMap
-      .subscribe(params => {
-        // console.log(params);
-        // params.keys // returns all the keys or route params
-        // params.get('id'); // returns string
-        let id = +params.get('id'); // the + converts to integer
-        console.log(id);
-      });
+    console.log("GitHubProfile OnInit");
+
+    // when you don't want user to stay on the same page and navigate
+    // back and forth. Then we use snpashot
+
+    let id = this.route.snapshot.paramMap.get('id'); // now this paramMap is the actual object and not observable
+    console.log(id);
+    
+    // this.route.paramMap
+    //   .subscribe(params => {
+    //     // console.log(params);
+    //     // params.keys // returns all the keys or route params
+    //     // params.get('id'); // returns string
+    //     let id = +params.get('id'); // the + converts to integer
+    //     console.log(id);
+    //   });
     // paramMap gives all the parameters. Type is Observable<ParamMap>
   }
 
